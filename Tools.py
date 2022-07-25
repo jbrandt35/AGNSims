4c28323ea1f3647974cd744a947e434c590b9cbe
joshua.brandt <joshua.brandt@ligo.org>
2022-07-25 13:37:37 -0400

    Moved bulky functions to tools and implemented a heartbeat function to replace the while-loop

--- Tools.py
+++ Tools.py
@@ -1,6 +1,7 @@
 import json
 import rebound
 import numpy as np
+from numpy.linalg import norm as mag
 import matplotlib.pyplot as plt
 from astropy import constants
 
@@ -160,7 +161,6 @@ def t_GW(sim, particle1, particle2):
     a = orbit.a
     e = orbit.e
 
-
     first_term = -64/5
     second_term = (sim.G ** 3) * (m1 + m2) * m1 * m2 / ((c**5) * (a**3) * ((1-e**2)**(7/2)))
     third_term = 1 + ((73/24)*(e**2)) + ((37/94)*(e**4))
@@ -168,7 +168,6 @@ def t_GW(sim, particle1, particle2):
     da_dt = first_term * second_term * third_term
 
     t_gw = np.abs(a/da_dt)
-
     return t_gw
 
 
@@ -231,4 +230,141 @@ def check_binary_bound(sim, outcome_record):
         outcome_record["Result"] = "Unbound"
         dump_record(outcome_record)
         raise Exception
+
+
+def dist_between(particle_1, particle_2):
+    particle_1_position, particle_2_position = np.array(particle_1.xyz), np.array(particle_2.xyz)
+    return mag(particle_1_position - particle_2_position)
+
+
+def check_and_assign_minimums(sim, record):
+    # Get binary COM as a rebound.Particle
+    binary_barycenter = sim.calculate_com(first = 1, last = 3)
+
+    distance_perturber_to_binary_COM = dist_between(sim.particles["perturber"], binary_barycenter)
+    distance_between_BBHs = sim.particles["BBH_2"].calculate_orbit(primary = sim.particles["BBH_1"]).d
+
+    t_gw = t_GW(sim, sim.particles["BBH_1"], sim.particles["BBH_2"])
+    relative_tGW = t_gw / get_binary_period(sim)
+
+    if distance_between_BBHs < record["Minimum Distance Between BBHs"]:
+        record["Minimum Distance Between BBHs"] = distance_between_BBHs
+    if distance_perturber_to_binary_COM < record["Minimum Distance Between Binary COM and Perturber"]:
+        record["Minimum Distance Between Binary COM and Perturber"] = distance_perturber_to_binary_COM
+    if t_gw < record["Minimum t_GW"]:
+        record["Minimum t_GW"] = t_gw
+    if relative_tGW < record["Minimum relative t_GW"]:
+        record["Minimum relative t_GW"] = relative_tGW
+
+def save_plotting_data(sim):
+    binary_barycenter = sim.calculate_com(first = 1, last = 3)
+
+    SMBH_distance = dist_between(sim.particles["SMBH"], binary_barycenter)
+    distance_between_BBHs = sim.particles["BBH_2"].calculate_orbit(primary = sim.particles["BBH_1"]).d
+
+    times.append(sim.t)
+    time_steps.append(sim.dt)
+    binary_distances.append(distance_between_BBHs)
+    m1_a_xs.append(sim.particles["BBH_1"].x - binary_barycenter.x)
+    m1_a_ys.append(sim.particles["BBH_1"].y - binary_barycenter.y)
+    m1_b_xs.append(sim.particles["BBH_2"].x - binary_barycenter.x)
+    m1_b_ys.append(sim.particles["BBH_2"].y - binary_barycenter.y)
+    SMBH_distances.append(SMBH_distance)
+    eccentricities.append(sim.particles["BBH_2"].calculate_orbit(primary = sim.particles["BBH_1"]).e)
+    SMBH_xs.append(sim.particles["SMBH"].x)
+    SMBH_ys.append(sim.particles["SMBH"].y)
+    perturber_xs.append(sim.particles["perturber"].x)
+    perturber_ys.append(sim.particles["perturber"].y)
+    m1_a_helio_xs.append(sim.particles["BBH_1"].x)
+    m1_a_helio_ys.append(sim.particles["BBH_1"].y)
+    m1_b_helio_xs.append(sim.particles["BBH_2"].x)
+    m1_b_helio_ys.append(sim.particles["BBH_2"].y)
+    perturber_m1a_distance.append(sim.particles["perturber"].calculate_orbit(primary = sim.particles["BBH_1"]).d)
+    perturber_m1b_distance.append(sim.particles["perturber"].calculate_orbit(primary = sim.particles["BBH_2"]).d)
+
+
+def construct_plots():
+
+    plt.plot(times, time_steps)
+    plt.xlabel("Time [yr]")
+    plt.ylabel("dt [yr]")
+    plt.title("Time-step over Time")
+    plt.yscale("log", base = 10)
+    plt.savefig("plots/timeVsdt.jpg", bbox_inches = "tight")
+    plt.close()
+
+    plt.plot(times, binary_distances)
+    plt.xlabel("Time [yr]")
+    plt.ylabel("Distance [AU]")
+    plt.title("Distance Between BHs in Binary over Time")
+    plt.savefig("plots/timeVSDistance.jpg", bbox_inches = "tight")
+    plt.close()
+
+    plt.plot(times, eccentricities)
+    plt.xlabel("Time [yr]")
+    plt.ylabel("Eccentricity")
+    plt.title("Eccentricity of the Binary in the Barycentric Frame over Time")
+    plt.savefig("plots/timeVSEccentricity.jpg", bbox_inches = "tight")
+    plt.close()
+
+    plt.scatter(m1_a_xs, m1_a_ys, label = "m1_a")
+    plt.scatter(m1_b_xs, m1_b_ys, label = "m1_b")
+    plt.title("Trajectory of Binary BBHs in Barycentric Frame")
+    plt.xlabel("x [AU]")
+    plt.ylabel("y [AU]")
+    plt.legend()
+    plt.savefig("plots/BinaryTrajectory.jpg", bbox_inches = "tight")
+    plt.close()
+
+    plt.plot(times, SMBH_distances)
+    plt.xlabel("Time [yr]")
+    plt.ylabel("Distance [AU]")
+    plt.title("Distance from SMBH to m1 over Time")
+    plt.savefig("plots/BinaryDisttoSMBH.jpg", bbox_inches = "tight")
+    plt.close()
+
+    plt.scatter(np.array(m1_a_helio_xs), np.array(m1_a_helio_ys), label = "m1_a")
+    plt.scatter(np.array(m1_b_helio_xs), np.array(m1_b_helio_ys), label = "m1_b")
+    plt.scatter(np.array(perturber_xs), np.array(perturber_ys), label = "perturber")
+    plt.scatter(0, 0, label = "SMBH")
+    plt.title("System Trajectory")
+    plt.xlabel("x [AU]")
+    plt.ylabel("y [AU]")
+    plt.legend()
+    plt.savefig("plots/SystemTrajectory.jpg", bbox_inches = "tight")
+    plt.close()
+
+    plt.plot(times, perturber_m1a_distance)
+    plt.title("Distance Between m1_a and m2 over Time")
+    plt.xlabel("Time [yr]")
+    plt.ylabel("Distance [AU]")
+    plt.savefig("plots/DistPerturbm1a.jpg", bbox_inches = "tight")
+    plt.close()
+
+    plt.plot(times, perturber_m1b_distance)
+    plt.title("Distance Between m1_b and m2 over Time")
+    plt.xlabel("Time [yr]")
+    plt.ylabel("Distance [AU]")
+    plt.savefig("plots/DistPerturbm1b.jpg", bbox_inches = "tight")
+    plt.close()
+
+#####################################################################################
+
+outcome_record = {"Result": None, "Minimum Distance Between Binary COM and Perturber": np.inf,
+                  "Minimum Distance Between BBHs": np.inf, "Minimum t_GW": np.inf, "Minimum relative t_GW": np.inf}
+
+total_time_steps_completed = 0
+
+time_steps, binary_distances, SMBH_distances, times, eccentricities = [], [], [], [], []
+
+m1_a_xs, m1_a_ys, m1_b_xs, m1_b_ys = [], [], [], []
+
+perturber_xs, perturber_ys = [], []
+SMBH_xs, SMBH_ys = [], []
+m1_a_helio_xs, m1_a_helio_ys = [], []
+m1_b_helio_xs, m1_b_helio_ys = [], []
+
+perturber_m1a_distance, perturber_m1b_distance = [], []
+
+#####################################################################################
 
