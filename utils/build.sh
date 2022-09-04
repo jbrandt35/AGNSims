c510e3ae653be8d83823a43e7574b81ca1ceab07
joshua.brandt <joshua.brandt@ligo.org>
2022-09-04 17:26:50 -0400

    Adding Slurm Support

--- utils/build.sh
+++ utils/build.sh
@@ -6,15 +6,15 @@ i=$3
 mkdir -p ../runs/BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}
 
 cp code/* ../runs/BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}
-cp submit.pbs ../runs/BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}
+cp submit.sbatch ../runs/BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}
 
 cd ../runs
 
-sed -i "s|NAME|B_${BBH_separation}_p_${perturber_separation}_${i}|g" BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}/submit.pbs
-sed -i "s|WORKDIR|/storage/home/hhive1/jbrandt35/data/AGN_sims/heatmap/runs/BBH_separation_${BBH_separation}/perturber_separation_${perturber_separation}/${i}|g" BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}/submit.pbs
+sed -i "s|NAME|B${BBH_separation}_p${perturber_separation}_${i}|g" BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}/submit.sbatch
+sed -i "s|WORKDIR|/storage/home/hhive1/jbrandt35/data/AGN_sims/Storage/slurm-test/runs/BBH_separation_${BBH_separation}/perturber_separation_${perturber_separation}/${i}|g" BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}/submit.sbatch
 
 sed -i "s|PERTSEPARATION|${perturber_separation}|g" BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}/Simulation.py
 sed -i "s|BINSEPARATION|${BBH_separation}|g" BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}/Simulation.py
 
-qsub BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}/submit.pbs
+sbatch BBH_separation_$BBH_separation/perturber_separation_$perturber_separation/${i}/submit.sbatch
 
--- utils/running_jobs.sh
+++ utils/running_jobs.sh
@@ -1,7 +1,6 @@
-qstat -u jbrandt35 > temp.txt
-sed -i '1,4d' temp.txt
+squeue -u jbrandt35 --noheader > temp.txt
 number_running=$(grep R -o temp.txt | wc -l)
-number_queued=$(grep Q -o temp.txt | wc -l)
+number_queued=$(grep PD -o temp.txt | wc -l)
 number=$(($number_running + $number_queued))
-echo $number
 rm temp.txt
+echo $number
