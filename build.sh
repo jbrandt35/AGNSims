1734e7d865a3fe97395e2d35bd96c80ae86617c2
joshua.brandt <joshua.brandt@ligo.org>
2022-09-29 14:42:34 -0400

    Merging Slurm into Master

---
+++ build.sh
@@ -0,0 +1,3 @@
+module load anaconda3/2022.05
+conda activate rebound_gr
+nohup python -u utils/Setup.py > output.log &
--- utils/running_jobs.sh
+++
@@ -1,6 +0,0 @@
-squeue -u jbrandt35 --noheader > temp.txt
-number_running=$(grep R -o temp.txt | wc -l)
-number_queued=$(grep PD -o temp.txt | wc -l)
-number=$(($number_running + $number_queued))
-rm temp.txt
-echo $number
