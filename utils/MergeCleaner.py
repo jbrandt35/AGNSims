b5a036cec77148868c7fd5abe00546ceb4632c0b
joshua.brandt <joshua.brandt@ligo.org>
2022-12-26 13:15:18 -0500

    Adding feature for MergeCleaner to regenerate and re-submit a job

--- utils/MergeCleaner.py
+++ utils/MergeCleaner.py
@@ -1,5 +1,6 @@
 import os
 import json
+from Setup import run_bash
 
 os.chdir("../runs/")
 
@@ -16,11 +17,11 @@ for BBH_separation_dir in [directory for directory in os.listdir() if "BBH_separ
                 outcome = "Didn't Finish"
 
             if "Collision Encountered" not in outcome:
-                os.system(f"rm -r {BBH_separation_dir}/{perturber_separation_dir}/{run_number}")
 
-                if len([directory for directory in
-                        os.listdir(os.path.join(BBH_separation_dir, perturber_separation_dir))]) == 0:
-                    os.system(f"rm -r {BBH_separation_dir}/{perturber_separation_dir}")
+                os.system(f"rm -r {BBH_separation_dir}/{perturber_separation_dir}/{run_number}/*")
 
-                if len([directory for directory in os.listdir(BBH_separation_dir)]) == 0:
-                    os.system(f"rm -r {BBH_separation_dir}")
+                BBH_separation = float(BBH_separation_dir.split("_")[-1])
+                perturber_separation = float(perturber_separation_dir.split("_")[-1])
+
+                job = f"{BBH_separation} {perturber_separation} {run_number}"
+                run_bash("build.sh", job)
