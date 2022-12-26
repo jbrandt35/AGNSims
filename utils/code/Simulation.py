b364d7c246ff6310ae38388d447c01274519bb54
joshua.brandt <joshua.brandt@ligo.org>
2022-12-26 13:24:55 -0500

    Making all binaries have inclination of 30 degrees

--- utils/code/Simulation.py
+++ utils/code/Simulation.py
@@ -11,7 +11,7 @@ mode = "initial_spin_aligned_with_L_of_BBH2"
 global inclination_of_binary
 
 sim = create_simulation()
-w = populate_simulation(sim, mode, perturber_a = perturber_a, binary_separation = binary_separation, ignore_perturber = True, randomize_M = True, binary_inc = np.radians(30))
+w = populate_simulation(sim, mode, perturber_a = perturber_a, binary_separation = binary_separation, ignore_perturber = True, randomize_M = True, binary_inc = inclination_of_binary)
 
 binary_period, SMBH_period, perturber_period = get_binary_period(sim), get_binary_SMBH_period(sim), get_perturber_period(sim)
 sim.dt = 0.05 * binary_period
