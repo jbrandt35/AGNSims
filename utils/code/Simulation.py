f9b632e58638ecc1a0607a924e28b0e6c24d4942
joshua.brandt <joshua.brandt@ligo.org>
2023-02-08 20:05:02 -0500

    cleaning up

--- utils/code/Simulation.py
+++ utils/code/Simulation.py
@@ -27,6 +27,8 @@ w = populate_simulation(sim, mode, perturber_a = perturber_a, binary_separation 
 binary_period, SMBH_period, perturber_period = get_binary_period(sim), get_binary_SMBH_period(sim), get_perturber_period(sim)
 sim.dt = 0.05 * binary_period
 
+##############################    Post-Newtonian Effects   #####################################
+
 rebx = reboundx.Extras(sim)
 
 #2.5 PN
